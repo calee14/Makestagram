25151fbb7830e244867d8cce28296e8fc592ea86
calee14 <caplee1000@gmail.com>
2018-07-05 15:28:57 -0700

    making login screen

---
+++ Makestagram/Controllers/LoginViewController.swift
@@ -0,0 +1,16 @@
+//
+//  LoginViewController.swift
+//  Makestagram
+//
+//  Created by Cappillen on 7/5/18.
+//  Copyright © 2018 Cappillen. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class LoginViewController: UIViewController {
+    override func viewDidLoad() {
+        
+    }
+}
