23817bc2fe7d96a3b273a7bfa01f3fb5589b32cd
calee14 <caplee1000@gmail.com>
2018-07-09 11:39:40 -0700

    added login feature creates user

--- Makestagram/Controllers/LoginViewController.swift
+++ Makestagram/Controllers/LoginViewController.swift
@@ -6,8 +6,11 @@
 //  Copyright © 2018 Cappillen. All rights reserved.
 //
 
-import Foundation
 import UIKit
+import FirebaseAuth
+import FirebaseUI
+
+typealias FIRUser = FirebaseAuth.User
 
 class LoginViewController: UIViewController {
     
@@ -19,6 +22,20 @@ class LoginViewController: UIViewController {
     }
     
     @IBAction func loginButtonTapped(_ sender: UIButton) {
-        print("Login Button Tapped")
+        guard let authUI = FUIAuth.defaultAuthUI() else { return }
+        authUI.delegate = self
+        
+        let authViewController = authUI.authViewController()
+        present(authViewController, animated: true)
+    }
+}
+
+extension LoginViewController: FUIAuthDelegate {
+    func authUI(_ authUI: FUIAuth, didSignInWith user: FIRUser?, error: Error?) {
+        if let error = error {
+            assertionFailure("Error signing in: \(error.localizedDescription)")
+            return
+        }
+        print("handle user signup / login")
     }
 }
