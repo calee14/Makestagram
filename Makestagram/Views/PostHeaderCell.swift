505c706a07370dc735c0378fe8e86124135cd967
calee14 <caplee1000@gmail.com>
2018-07-11 17:00:12 -0700

    working on adding header for post

---
+++ Makestagram/Views/PostHeaderCell.swift
@@ -0,0 +1,16 @@
+//
+//  PostHeaderCell.swift
+//  Makestagram
+//
+//  Created by Cappillen on 7/11/18.
+//  Copyright © 2018 Cappillen. All rights reserved.
+//
+
+import UIKit
+
+class PostHeaderCell: UITableViewCell {
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+    }
+}
