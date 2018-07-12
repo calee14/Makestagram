699c84bf173dc0531af4b20d8c53cf442a2464e1
calee14 <caplee1000@gmail.com>
2018-07-12 14:03:10 -0700

    working on find friends feature

---
+++ Makestagram/Services/FollowService.swift
@@ -0,0 +1,63 @@
+//
+//  FollowService.swift
+//  Makestagram
+//
+//  Created by Cappillen on 7/12/18.
+//  Copyright © 2018 Cappillen. All rights reserved.
+//
+
+import Foundation
+import FirebaseDatabase
+
+struct FollowService {
+    private static func followUser(_ user: User, forCurrentUserWithSuccess success: @escaping (Bool) -> Void) {
+        let currentID = User.current.uid
+        let followData = ["followers/\(user.uid)/\(currentID)": true,
+                          "following/\(user.uid)/\(currentID)": true]
+        let ref = Database.database().reference()
+        ref.updateChildValues(followData) { (error, _) in
+            if let error = error {
+                assertionFailure(error.localizedDescription)
+            }
+            
+            success(error == nil)
+        }
+        
+    }
+    
+    private static func unfollowUser(_ user: User, forCurrentUserWithSuccess success: @escaping (Bool) -> Void) {
+        let currentUID = User.current.uid
+        let followData = ["followers/\(user.uid)/\(currentUID)": NSNull(),
+                          "following/\(user.uid)/\(currentUID)": NSNull()]
+        let ref = Database.database().reference()
+        ref.updateChildValues(followData) { (error, ref) in
+            if let error = error {
+                assertionFailure(error.localizedDescription)
+            }
+            
+            success(error == nil)
+        }
+        
+    }
+    
+    static func setIsFollowing(_ isFollowing: Bool, fromCurrentUserTo followee: User, success: @escaping (Bool) -> Void) {
+        if isFollowing {
+            followUser(followee, forCurrentUserWithSuccess: success)
+        } else {
+            unfollowUser(followee, forCurrentUserWithSuccess: success)
+        }
+    }
+    
+    static func isUserFollowed(_ user: User, byCurrentUserWithCompletion completion: @escaping (Bool) -> Void) {
+        let currentUID = User.current.uid
+        let ref = Database.database().reference().child("followers").child(user.uid)
+        
+        ref.queryEqual(toValue: nil, childKey: currentUID).observeSingleEvent(of: .value, with: { (snapshot) in
+            if let _ = snapshot.value as? [String: Bool] {
+                completion(true)
+            } else {
+                completion(false)
+            }
+        })
+    }
+}
